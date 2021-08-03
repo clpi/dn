5eaa1f186de669a8250d3dd101cd2ea5e60675f2
Chris Pecunies <clp@clp.is>
2021-08-03 04:15:24 -0700

    adding more routes, deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
+// dnet experimental service
+// other services: io/data sevice, p2p service, crawler
+pub mod error;
+
 use axum::{
-    prelude::*, response::IntoResponse,
+    extract::FromRequest, prelude::*, 
+    response::{Html, IntoResponse, Json},
 };
+use serde_json::{json, Value};
+use std::{io, collections::HashMap};
 use serde::{Serialize, Deserialize};
-use std::io;
 
 pub async fn init() -> io::Result<()> {
     tracing_subscriber::fmt::init();
-    let app = route("/", get(root))
-        .route("/users", post(create_user));
+    let mut app = route("/", get(index))
+        .route("/health", get(health))
+        .route("/auth", get(get_auth))
+        .route("/group", get(get_groups))
+        .route("/users", get(get_users).post(create_user).delete(clear_users))
+        .route("/channel", get(health))
+        .route("/user/:id", get(get_user_by_id));
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
     hyper::Server::bind(&addr)
@@ -17,8 +28,14 @@ pub async fn init() -> io::Result<()> {
     Ok(())    
 }
 
+async fn get_auth(tok: ApiToken) -> String {
+    format!("Token is {:#?}", &tok)
+}
+async fn index() -> Html<&'static str> {
+    axum::response::Html("<h1>dnet index</h1>")
+}
 
-async fn root() -> &'static str {
+async fn health() -> &'static str {
     "hi there"
 }
 
@@ -33,10 +50,61 @@ async fn create_user(
     (http::StatusCode::CREATED, response::Json(user))
 }
 
+async fn get_users(_query: extract::Query<HashMap<String, String>>) -> Json<Value> {
+    Json(json!(  { "user": "user", "email": "user@email.com", "id": 1 }))
+
+}
+
+async fn get_user_by_id() -> Result<Json<Value>, http::StatusCode> {
+    Ok(Json(json!( {"user": 1})))
+}
+
+async fn clear_users() {}
+
+async fn get_groups(_query: Option<extract::Query<GroupQuery>>) {}
+
 #[derive(Serialize, Default, Deserialize)]
 struct User {
     #[serde(skip_serializing)]
     id: u64,
     email: String,
     username: String,
+}
+
+#[derive(Debug)]
+pub struct ApiToken(http::HeaderValue);
+
+impl std::ops::Deref for ApiToken {
+    type Target = http::HeaderValue;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl AsRef<http::HeaderValue> for ApiToken {
+    fn as_ref(&self) -> &http::HeaderValue {
+        &self.0
+    }
+}
+
+#[async_trait::async_trait]
+impl<B: Send> FromRequest<B> for ApiToken {
+    type Rejection = (http::StatusCode, &'static str);
+
+    async fn from_request(req: &mut extract::RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let token = req.headers()
+            .and_then(|headers| headers.get("DNET_API_TOKEN"));
+        if let Some(token) = token {
+            Ok(ApiToken(token.clone()))
+        } else {
+            Err((http::StatusCode::BAD_REQUEST, "No api token"))
+        }
+        
+    }
+}
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct GroupQuery {
+    group_id: usize,
+    user_id: usize,
+    email: String,
 }
